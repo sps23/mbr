3f82039794980b36c479e910e7c5882f037ca99c
Sylwester Stocki <sylwester.stocki.rnd@gmail.com>
2016-06-04 18:21:43 +0100

    Fix for infinity distance.

--- src/main/scala/silverstar/mbr/statistics/Infinity.scala
+++ src/main/scala/silverstar/mbr/statistics/Infinity.scala
@@ -4,16 +4,20 @@ import silverstar.mbr.model.DataPoint
 
 case class Infinity() extends Distance {
 
+  override def distance(x: DataPoint, y: DataPoint): Option[Double] =
+    distance(x.toList(), y.toList())
+
   override def distance(x: List[Double], y: List[Double]): Option[Double] = {
     if (x.length == y.length) {
       if (x.isEmpty) None
-      else Some((x, y).zipped.map(_ - _).max)
+      else {
+        val diffAbs: List[Double] = (x, y).zipped.map((xx, yy) => Math.abs(xx - yy))
+        val maxOfDiffAbs: Double = diffAbs.max
+        Some(maxOfDiffAbs)
+      }
     }
     else None
   }
-
-  override def distance(x: DataPoint, y: DataPoint): Option[Double] =
-    distance(x.toList(), y.toList())
 }
 
 object Infinity extends Infinity
